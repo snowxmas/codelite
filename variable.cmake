7d13f93cd55bdb37d5d69e8f22e43373057b2f3f
mayuchao <mayuchao@wisetv.com.cn>
2018-05-23 17:22:10 +0800

    Enable clang code-completion by force.

--- variable.cmake
+++ variable.cmake
@@ -5,5 +5,6 @@ set( CMAKE_BUILD_TYPE "Release" )
 set( ENABLE_SFTP 0 )
 set( WITH_WXC 1 )
 set( WITH_FLEX 1 )
+set( ENABLE_CLANG 1 )
 set( ENABLE_LLDB 0 )
 #############################################
